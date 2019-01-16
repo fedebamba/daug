edc7a7c46644358be1abc859a754ba408a97f79a
federico <federicobambagioni@gmail.com>
2019-01-16 16:33:29 +0100

    init

---
+++ acquisition_functions.py
@@ -0,0 +1,65 @@
+import torch.nn.functional as F
+import torch as T
+
+import numpy
+
+
+def entropy(net_out):
+    sm = F.softmax(net_out)
+    logsm = T.log(sm)
+    print(logsm.size())
+    return -T.sum(sm * logsm, 1)
+
+
+def avg_entropy(net_out):
+    e = numpy.zeros(shape=len(net_out[0]))
+    for el in net_out:
+        sm = F.softmax(el)
+        logsm = numpy.log2(sm)
+        f = T.mul(T.tensor(sm).to("cuda:0"), T.tensor(logsm).to("cuda:0")).double()
+        e = e - T.sum(f.cpu(), 1)
+    return e/len(net_out)
+
+
+def confidence(vector, num_of_classes=10, details=False):
+    if not details:
+        return [max([(len([i[v] for i in vector if i[v] == j])) for j in range(num_of_classes)]) for v in range(len(vector[0]))]
+    else:
+        res = [[(len([i[v] for i in vector if i[v] == j])) for j in range(num_of_classes)] for v in range(len(vector[0]))]
+        print(res)
+        return res
+
+
+
+def avg_KL_divergence():
+    pass
+
+
+
+
+def max_variance(net_out):
+    # it's the same as bestofn
+    pred = [out.max(1)[1] for out in net_out]
+    return 1 - (confidence(pred)/len(net_out))
+
+
+    #   e = numpy.zeros(shape=(len(net_out[0]), 10))
+    # for el in net_out:
+    #    sm = F.softmax(el)
+    #    e = T.dist(e, sm, 1)
+    #    print(e)
+
+    # return e.max(1)
+
+
+def entropic_distance(net_out):
+    sm = F.softmax(net_out[0])
+    logsm = numpy.log2(sm)
+    e = - T.mul(T.tensor(sm).to("cuda:0"), T.tensor(logsm).to("cuda:0")).double().cpu()
+
+    sm = F.softmax(net_out[1])
+    logsm = numpy.log2(sm)
+    f = - T.mul(T.tensor(sm).to("cuda:0"), T.tensor(logsm).to("cuda:0")).double().cpu()
+
+    ee = T.sum((e - f).abs(), 1)
+    return ee
---
+++ main_cifar_2.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as tud
+
+import torchvision.transforms as trans
+
+import numpy
+import copy
+import csv
+import datetime
+
+import prova_torch_resnet as netter
+import customcifar
+import net_functions as nf
+import semi_supervised
+
+num_of_classes = 10
+val_percentage = .2
+
+initial_percentage = .3
+iteration_step = .1
+
+learning_rate = 0.001
+num_of_epochs = 10
+
+
+
+transform = trans.Compose([
+        # trans.RandomRotation(5),
+        # trans.RandomCrop(26),
+        # trans.Resize((32, 32)),
+        # utils.Gauss(0, 0.05),
+        trans.ToTensor()
+    ])
+
+
+class CompleteDataset:
+    def __init__(self):
+        self.dataset = customcifar.CustomCIFAR10(root="./cifar", train=True, download=True, transform=transform)
+        self.testset = customcifar.CustomCIFAR10(root="./cifar", train=False, download=True, transform=transform) # palindromo!
+
+        dataloader = tud.DataLoader(self.dataset, batch_size=64, shuffle=False, num_workers=2,
+                                    sampler=customcifar.CustomRandomSampler([x for x in range(len(self.dataset))]))
+        el_for_class = [[] for x in range(num_of_classes)]
+        for batch_index, (inputs, targets, index) in enumerate(dataloader):
+            for t in range(len(targets)):
+                el_for_class[targets[t]].append(index[t].item())
+
+        val_els_per_class = int((len(self.dataset) * val_percentage) / num_of_classes)
+
+        self.validation_indices = [el for xl in el_for_class for el in numpy.random.choice(xl, size=val_els_per_class, replace=False)]
+        self.remaining_indices = [x for x in range(len(self.dataset)) if x not in self.validation_indices]
+        self.train_indices = numpy.random.choice(self.remaining_indices, size=int(len(self.remaining_indices)*initial_percentage  ), replace=False)
+
+        print("Dataset loaded: train length {0}/{3} | validation length {1} | test length {2}".format(len(self.train_indices), len(self.validation_indices), len(self.testset), len(self.remaining_indices)))
+
+    def add_to_train(self, indices):
+        indices = [x for x in indices if x in self.remaining_indices and x not in self.train_indices]
+        self.train_indices.resize(len(self.train_indices) + len(indices))
+        self.train_indices[-len(indices):] = indices
+
+    def get_train_loader(self):
+        return tud.DataLoader(self.dataset, batch_size=64, shuffle=False, num_workers=2, sampler=customcifar.CustomRandomSampler(self.train_indices))
+
+    def get_validation_loader(self):
+        return tud.DataLoader(self.dataset, batch_size=64, shuffle=False, num_workers=2,
+                              sampler=customcifar.CustomRandomSampler(self.validation_indices))
+
+    def get_test_loader(self):
+        return tud.DataLoader(self.testset, batch_size=64, shuffle=False, num_workers=2,
+                              sampler=customcifar.CustomRandomSampler([x for x in range(len(self.testset))]))
+
+
+def new_net():
+    net = netter.CustomResNet18()
+    net = net.to("cuda:0")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-4)
+
+    return nf.NetTrainer(net=net, criterion=criterion, optimizer=optimizer)
+
+
+def single_train_pass(cd):
+    print("Epoch: " + str(i))
+    trainloader = cd.get_train_loader()
+    validationloader = cd.get_validation_loader()
+
+    net = new_net()
+    best_net = net.clone()
+    for i in range(num_of_epochs):
+        net.train(i, trainloader)
+        isbest, acc = net.validate(i, validationloader)
+        print("Accuracy so far: {0:.2f}".format(acc))
+
+        if isbest:
+            best_net = net.clone()
+
+    return best_net
+
+
+
+# ====================================================================== #
+# ---------------------------------------------------------------------- #
+# ====================================================================== #
+
+
+
+cd = CompleteDataset()
+
+ind = [x for x in cd.remaining_indices if x not in cd.train_indices][:int(len(cd.remaining_indices)*.1)]
+
+
+print(len(cd.train_indices))
+
+net = single_train_pass(cd)
+
+print("\n\t  TEST:")
+best_acc =  net.test(0, cd.get_test_loader())
+print("Test accuracy: {0:.2f}".format(best_acc))
+
+
+for iteration_index in numpy.arange(initial_percentage, 1, iteration_step):
+    ind = [x for x in cd.remaining_indices if x not in cd.train_indices][:int(len(cd.remaining_indices)*iteration_step)] # active learning methods here?
+    cd.add_to_train(ind)
+    print(len(cd.train_indices))
+
+    # get the weak labels with semi_supervised.generate_weak_labels
+    semi_supervised.generate_weak_labels(net=net.net, cds=cd, indices=[x for x in cd.remaining_indices if x not in cd.train_indices], howmany=1000, train_indices=[])
+
+    # train the control network
+    net = single_train_pass(cd)
+
+    # train the semi supervised learning network
+    # todo
+
+
+
+
