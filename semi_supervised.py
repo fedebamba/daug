569aaa96a4b82f876908a8412fb17be2b477f9ab
fedebamba <federicobambagioni@gmail.com>
2019-01-24 17:59:14 +0100

    Update semi_supervised.py

--- semi_supervised.py
+++ semi_supervised.py
@@ -76,5 +76,7 @@ def generate_weak_labels(net, cds, indices, train_indices, n=5):
         normalized_confidence[0] = c_max[0]
         normalized_confidence[1] = c_max[1]
 
+        print(torch.sort(normalized_confidence[0])[1])
+        
         return torch.cat((normalized_confidence[0].reshape(len(normalized_confidence[0]), 1), normalized_confidence[1].reshape(len(normalized_confidence[1]), 1), normalized_confidence[2].reshape(len(normalized_confidence[2]), 1)), 1)
 
