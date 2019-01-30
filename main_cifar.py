6ac1dff79cc1b9e92e67e7030881c501a42badc8
fedebamba <federicobambagioni@gmail.com>
2019-01-30 13:23:12 +0100

    Update main_cifar.py

--- main_cifar.py
+++ main_cifar.py
@@ -189,7 +189,7 @@ def a_single_experiment(esname, esnumber):
     for i in range(first_time_multiplier, until_slice_number):
         active_indices = active_net.distance_and_entropy(dataset,
                                                      [x for x in dataset.train_indices if x not in el_for_active], tslp,
-                                                     el_for_active, n=1)
+                                                     el_for_active, n=5)
 
 
         normal_indices = numpy.random.choice([x for x in dataset.train_indices if x not in el_for_normal], size=tslp, replace=False)
