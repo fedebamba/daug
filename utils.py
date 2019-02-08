5bfc54886c0a9530aaf733ebad6e6ccf9a228640
fedebamba <federicobambagioni@gmail.com>
2019-02-08 16:16:53 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -53,7 +53,8 @@ def prettyprint(conf, level=0):
             print(("\t"*level) + "- " + k + ":")
             prettyprint(v, level+1)
         else:
-            print(("\t"*level) + "- \033[94m" + k + "\033[0m : \033[92m" + "{:<25}".format(v) + "\033[0m ")
+            vv = "{:25}".format(v)
+            print(("\t"*level) + "- \033[94m" + k + "\033[0m : \033[92m" + vv + "\033[0m ")
 
 
 
