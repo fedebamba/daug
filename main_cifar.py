65036d3d6831d7f199c4d29c2318f887814dcf43
fedebamba <federicobambagioni@gmail.com>
2019-02-12 13:38:17 +0100

    Update main_cifar.py

--- main_cifar.py
+++ main_cifar.py
@@ -38,10 +38,13 @@ af_config= {
 trans_conf = utils.checkconf(conf_file, "trans_config", None)
 trans_list = []
 if utils.checkconf(trans_conf, "rotation", True):
+    pass
     # trans_list.append(trans.RandomRotation(utils.checkconf(trans_conf, "rotation_degree", 5)))
 if utils.checkconf(trans_conf, "crop", True):
+    pass
     # trans_list.append(trans.RandomCrop(utils.checkconf(trans_conf, "crop_amount", 26)))
 if utils.checkconf(trans_conf, "gauss", True):
+    pass
     # trans_list.append(utils.Gauss(utils.checkconf(trans_conf, "gauss_mean", 0), utils.checkconf(trans_conf, "gauss_var", 0.1)))
 trans_list.append(trans.Resize((32, 32)))
 trans.ToTensor()
