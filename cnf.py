6649e6dc331364d99c0547c00f100b332a183975
fedebamba <federicobambagioni@gmail.com>
2019-02-18 15:25:12 +0100

    Update cnf.py

--- cnf.py
+++ cnf.py
@@ -620,6 +620,37 @@ stuff = {
             "gauss_var": 0.1
         }
     },
+    "uuu-entropy-new-rotation_daug-crop_daug-flip_daug": {
+        "af_config": {
+            "using_ensemble_entropy": False,
+            "varratio_weight": 0,
+            "distance_weight": 1e-7,
+            "using_max": False
+        },
+        "using_prior": False,
+        "prior_baseline": False,
+        "balanced": "uuu",
+        "el_for_validation": .1,
+        "execute_random": False,
+        "daug": True,
+        "n": 7,
+        "num_of_runs": 6,
+        "trans_config": {
+            "rotation_degree": 7,
+            "crop_amount": 24,
+            "gauss_mean": 0,
+            "gauss_var": 0.05
+        },
+        "selection_trans_config": {
+            "rotation_degree": 7,
+            "crop_amount": 24,
+            "flip": True,
+            "gauss": False
+        }
+    },
+
+
+
 }
 
 
