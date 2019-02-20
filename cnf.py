c9104bff54f82d49dda348c1df3de476b3486983
fedebamba <federicobambagioni@gmail.com>
2019-02-20 20:33:43 +0100

    Update cnf.py

--- cnf.py
+++ cnf.py
@@ -734,8 +734,65 @@ stuff = {
             "gauss": False
         }
     },
+    "uuu-ee-new-rotation_daug": {
+        "af_config": {
+            "using_ensemble_entropy": True,
+            "varratio_weight": 0,
+            "distance_weight": 1e-7,
+            "using_max": False
+        },
+        "using_prior": False,
+        "prior_baseline": False,
+        "balanced": "uuu",
+        "el_for_validation": .1,
+        "execute_random": False,
+        "daug": True,
+        "n": 7,
+        "num_of_runs": 6,
+        "trans_config": {
+            "rotation_degree": 7,
+            "crop_amount": 24,
+            "gauss_mean": 0,
+            "gauss_var": 0.05
+        },
+        "selection_trans_config": {
+            "rotation_degree": 5,
+            "crop": False,
+            "flip": False,
+            "gauss": False
+        }
+    },
+    "uuu-ee-new-crop_daug": {
+        "af_config": {
+            "using_ensemble_entropy": True,
+            "varratio_weight": 0,
+            "distance_weight": 1e-7,
+            "using_max": False
+        },
+        "using_prior": False,
+        "prior_baseline": False,
+        "balanced": "uuu",
+        "el_for_validation": .1,
+        "execute_random": False,
+        "daug": True,
+        "n": 7,
+        "num_of_runs": 6,
+        "trans_config": {
+            "rotation_degree": 7,
+            "crop_amount": 24,
+            "gauss_mean": 0,
+            "gauss_var": 0.05
+        },
+        "selection_trans_config": {
+            "rotation": False,
+            "crop_amount": 24,
+            "flip": False,
+            "gauss": False
+        }
+    },
 
 }
+
 
 
 
