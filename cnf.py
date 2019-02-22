6cf1dd847de4a96f1cb2878452e3c8cbc37733cf
fedebamba <federicobambagioni@gmail.com>
2019-02-22 18:50:28 +0100

    Update cnf.py

--- cnf.py
+++ cnf.py
@@ -938,6 +938,36 @@ stuff = {
         }
     },
 
+    "uuu-entropy-test-for-exclusive-transformations": {
+        "af_config": {
+            "using_ensemble_entropy": False,
+            "varratio_weight": 0,
+            "distance_weight": 1e-7,
+            "using_max": False
+        },
+        "using_prior": False,
+        "prior_baseline": False,
+        "balanced": "uuu",
+        "el_for_validation": .1,
+        "execute_random": False,
+        "daug": True,
+        "n": 9,
+        "num_of_runs": 6,
+        "trans_config": {
+            "rotation_degree": 7,
+            "crop_amount": 24,
+            "gauss_mean": 0,
+            "gauss_var": 0.05
+        },
+        "selection_trans_config": {
+            "exclusive_transformations": True,
+            "rotation_degree": 4,
+            "crop_amount": 26,
+            "flip": False,
+            "gauss_mean": 0,
+            "gauss_var": 0.01
+        }
+    },
 
 }
 
