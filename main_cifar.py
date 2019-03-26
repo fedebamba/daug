92ca9591c9b0497d426ac017825430d0f47c1aff
fedebamba <federicobambagioni@gmail.com>
2019-03-26 16:09:07 +0100

    Update main_cifar.py

--- main_cifar.py
+++ main_cifar.py
@@ -241,8 +241,8 @@ def a_single_experiment(esname, esnumber, seed):
     numpy.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = False
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     with open("res/results_{0}_{1}.csv".format(esname, esnumber), "w") as csvfile:
         writer = csv.writer(csvfile)
