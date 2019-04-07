bedffcb7131193803f1674db17afcbbbb21e6f99
fedebamba <federicobambagioni@gmail.com>
2019-04-07 13:50:15 +0200

    Update main_cifar.py

--- main_cifar.py
+++ main_cifar.py
@@ -162,7 +162,7 @@ class CifarLoader():
              # self.already_selected_indices = numpy.random.choice(self.train_indices, size=tslp*first_time_multiplier, replace=False).tolist()
              if starting_indexes is not None:
                  print(starting_indexes)
-                 self.already_selected_indices = starting_indexes
+                 self.already_selected_indices = [x for x in starting_indexes]
              else:
                 self.already_selected_indices = self._train_val_set.define_starting_set(forced_distribution=True)
         else:
